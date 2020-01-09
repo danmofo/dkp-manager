6095f9248554e7ba909109b8dfc14815ec101323
Daniel Moffat <danmofo@gmail.com>
2020-01-09 17:47:51 +0000

    Playing around with sharing the class files directly with the container so we do not need to perform a Docker build during development, this takes 20s even on a good machine which is just not acceptable for me...

--- server/src/main/java/com/dmoffat/dkpmanager/Application.java
+++ server/src/main/java/com/dmoffat/dkpmanager/Application.java
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class Application {
     public static void main(String[] args) {
+        System.out.println("Hello world!2");
         SpringApplication.run(Application.class, args);
     }
 }
